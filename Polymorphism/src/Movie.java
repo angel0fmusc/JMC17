a7c1cd0476a06d01fec41d39b93487095b180344
angel0fmusc <angel0fmusic.vr@gmail.com>
2024-10-31 16:12:51 -0400

    Updated Polymorphism project

---
+++ Polymorphism/src/Movie.java
@@ -0,0 +1,83 @@
+public class Movie {
+
+    private String title;
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public void watchMovie(){
+
+        String instanceType = this.getClass().getSimpleName();
+        System.out.println(title + " is a " + instanceType + " film");
+    }
+
+    public static Movie getMovie(String type, String title){
+        return switch(type.toUpperCase().charAt(0)){
+            case 'A' -> new Adventure(title);
+            case 'C' -> new Comedy(title);
+            case 'S' -> new SciFi(title);
+            default -> new Movie(title);
+        };
+    }
+}
+
+class Adventure extends Movie{
+
+    public Adventure(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf("..%s%n".repeat(3),
+                "Pleasant Scene",
+                "Scary music",
+                "Something Bad Happens");
+    }
+
+    public void watchAdventure(){
+        System.out.println("Watching an Adventure!");
+    }
+}
+
+class Comedy extends Movie{
+
+    public Comedy(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf("..%s%n".repeat(3),
+                "Something funny happens",
+                "Something even funnier happens",
+                "Happy ending");
+    }
+
+    public void watchComedy(){
+        System.out.println("Watching an Comedy!");
+    }
+}
+
+class SciFi extends Movie{
+
+    public SciFi(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf("..%s%n".repeat(3),
+                "Bad Aliens do bad stuff",
+                "Space guys chase aliens",
+                "Planet blows up");
+    }
+
+    public void watchSciFi(){
+        System.out.println("Watching a SciFi!");
+    }
+}
