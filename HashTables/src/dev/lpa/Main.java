007516d7622b83ec966b4d6a7f231209c4508669
angel0fmusc <angel0fmusic.vr@gmail.com>
2024-11-10 16:38:40 -0500

    Updated hash table main

--- HashTables/src/dev/lpa/Main.java
+++ HashTables/src/dev/lpa/Main.java
@@ -8,9 +8,11 @@ public class Main {
         HashTable myHashTable = new HashTable(50);
         myHashTable.set("grapes", 10000);
         myHashTable.set("apples", 54);
+        myHashTable.set("oranges", 2);
 
         System.out.println("value for key grapes: " + myHashTable.get("grapes"));
         System.out.println("value for key apple: " + myHashTable.get("apples"));
+        System.out.println("value for key apple: " + myHashTable.get("oranges"));
         System.out.println("list of keys: " + Arrays.toString(myHashTable.keys()));
     }
 }
