00f8b2b92e96a33fc6b6b352070281f5f1d6e4ca
Victoria Rivas <angel0fmusic.vr@gmail.com>
2025-01-29 11:40:19 -0500

    Insertion sort changes
    Removed print statements, relocated removal of current value from the unsorted section after it has been sorted.

--- Sorting/src/dev/interview/algorithms/Main.java
+++ Sorting/src/dev/interview/algorithms/Main.java
@@ -14,10 +14,9 @@ public class Main {
         System.out.print("Original: ");
         System.out.println(Arrays.toString(unsortedNumbers));
 
-//        System.out.println("Bubble Sort: " + Arrays.toString(myBubbleSort(unsortedNumbers)));
-//        System.out.println("Selection Sort: " + Arrays.toString(selectionSort(unsortedNumbers)));
-        System.out.println("\nInsertion Sort with LinkedList:");
-        insertionSort(unsortedNumbers);
+        System.out.println("Bubble Sort: " + Arrays.toString(myBubbleSort(unsortedNumbers)));
+        System.out.println("Selection Sort: " + Arrays.toString(selectionSort(unsortedNumbers)));
+        System.out.println("Insertion Sort with LinkedList: " + Arrays.toString(insertionSort(unsortedNumbers)));
 
     }
 
@@ -71,14 +70,22 @@ public class Main {
         return sortedNumbers;
     }
 
+    /**
+     * Insertion sort using LinkedList. Converts and Integer[] array into
+     * a LinkedList of the same type. Traverses the LinkedList and
+     * adds the current value to the beginning if the current value is less than the smallest sorted
+     * number. Otherwise, traverse either up or down the sorted section based on the value
+     * at the median index. ToDo: move the swapping mechanism to a separate method
+     * @param nums an unsorted Integer[] array
+     * @return the sorted LinkedList as an Array
+     */
     public static Integer[] insertionSort(Integer[] nums){
         /*
          * Convert the array to a LinkedList using Arrays.asList() and the LinkedList constructor.
          * Arrays.asList() creates a fixed-size list backed by the specified array,
          * and the LinkedList constructor then takes this list to initialize a new LinkedL ist.
          */
-        var sortedNums = new LinkedList<Integer>(Arrays.asList(nums));
-        System.out.println("Size of the array = " + sortedNums.size());
+        var sortedNums = new LinkedList<>(Arrays.asList(nums));
         for(int i = 1; i < sortedNums.size(); i++){
             // If the current element is less than the first, move to the beginning
             if(sortedNums.get(i) <= sortedNums.getFirst()){
@@ -101,38 +108,31 @@ public class Main {
                 *  Otherwise, traverse up the LinkedList from median to i-1.
                 */
                 int medianIndex = (1 + (i-1))/2;
-                System.out.println("medianIndex = " + medianIndex);
                 if(sortedNums.get(i) < sortedNums.get(medianIndex)){
-                    System.out.println("Traversing down from middle index to 1.");
                     for(int j = medianIndex; j >= 1; j--){
-                        System.out.printf("sortedNums[%d]=%5d%n", j, sortedNums.get(j)); // Printing out for now
                         /* Check if current value is (sortedNums.get(i)) is greater than
                         * sortedNums.get(j), put the current value in sortedNums.get(j+1)
                         * */
                         if(sortedNums.get(i) >= sortedNums.get(j)){
                             sortedNums.add(j+1,sortedNums.get(i));
-                            sortedNums.remove(i+1);
                             break;
                         }
                     }
                 } else{
-                    System.out.println("Traversing up from middle index to i-1.");
                     // Traverse up from the medianIndex to i-1
                     for(int j = medianIndex; j <= i-1; j++){
                         /* Check if the current value is less than sortedNums.get(j).
                         * If so, put the current value before sortedNums.get(j);
-                        * sortedNums.get(j-1)*/
-                        System.out.printf("sortedNums[%d]=%5d%n", j, sortedNums.get(j));
+                        * sortedNums.get(j-1)
+                        * */
                         if(sortedNums.get(i) <= sortedNums.get(j)){
                             sortedNums.add(j,sortedNums.get(i));
-                            sortedNums.remove(i+1);
                             break;
                         }
                     }
                 }
+                sortedNums.remove(i+1);
             }
-            System.out.printf("i = %d\t",i);
-            System.out.println(sortedNums);
         }
         return sortedNums.toArray(new Integer[0]);
     }
