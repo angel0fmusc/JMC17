73186b0c74e2883ad480a4422868e4f6f579b169
Victoria Rivas <angel0fmusic.vr@gmail.com>
2025-04-14 15:40:18 -0400

    BlackJack hit, basic gameplay
    Added hit method and adjusted printing on Hand class. Added user input via Scanner class and loop through Player gameplay.

--- CardGameChallenge/src/dev/lpa/Hand.java
+++ CardGameChallenge/src/dev/lpa/Hand.java
@@ -5,9 +5,10 @@ import java.util.List;
 
 public class Hand {
 
-    private List<Card> hand = new ArrayList<>();
+    private final List<Card> hand = new ArrayList<>();
     private int handValue = 0;
-    private final boolean dealer;
+    private boolean dealer;
+    private boolean stand;
 
     public Hand(List<Card> deck, boolean isDealer) {
         if(!deck.isEmpty()){
@@ -18,6 +19,7 @@ public class Hand {
             }
         }
         dealer = isDealer;
+        stand = false;
     }
 
     public List<Card> getHand() {
@@ -28,13 +30,46 @@ public class Hand {
         return handValue;
     }
 
-    public void printHand(){
+    public boolean isStand() {
+        return stand;
+    }
+
+    public void setStand(boolean stand) {
+        this.stand = stand;
+    }
+
+    public void setDealer(boolean dealer) {
+        this.dealer = dealer;
+    }
+
+    /**
+     * Hit mechanics. If the provided deck is not empty
+     * Then remove 1 card. If that card is an Ace and the initial
+     * rank of 11 causes the player to bust, then automatically
+     * adjust the rank to 1.
+     * @param deck a List of Cards
+     */
+    public void hit(List<Card> deck){
+        if(!deck.isEmpty()){
+            Card tempCard = deck.remove(0);
+            if(tempCard.face().equals("A") && handValue + tempCard.rank() > 21){
+                // adjust the card rank
+                tempCard = new Card(tempCard.suit(), tempCard.face(), 1);
+            }
+            hand.add(tempCard);
+            handValue += tempCard.rank();
+            return;
+        }
+        System.out.println("Deck is empty");
+    }
+
+    public void printHand(String description){
         if(dealer){
-            Card.printDeck(List.of(hand.get(0)), "Dealer's hand", 1);
+            Card.printDeck(List.of(hand.get(0)), description + "'s hand", 1);
             return;
         }
 
-        Card.printDeck(hand, "Player's hand (" + handValue + ")", 1);
+        Card.printDeck(hand, description + "'s hand (" + handValue + ")", 1);
     }
 
 }
--- CardGameChallenge/src/dev/lpa/Main.java
+++ CardGameChallenge/src/dev/lpa/Main.java
@@ -2,13 +2,16 @@ package dev.lpa;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
+    public static List<Card> deck = Card.getStandardDeck();
+
     public static void main(String[] args) {
 
-        List<Card> deck = Card.getStandardDeck();
-        Card.printDeck(deck);
+        Scanner scanner = new Scanner(System.in);
+//        Card.printDeck(deck);
 
         Collections.shuffle(deck);
 
@@ -17,14 +20,63 @@ public class Main {
         * */
         Hand houseHand = new Hand(deck, true);
         Hand playerHand = new Hand(deck, false);
-//        Card.printDeck(houseHand.getHand(), "Dealer's hand ("+ houseHand.getHandValue()+ ")", 1);
-//        Card.printDeck(playerHand.getHand(), "Player's hand", 1);
 
-        houseHand.printHand();
-        playerHand.printHand();
+        houseHand.printHand("Dealer");
+        playerHand.printHand("Player");
 
         System.out.println("Does Dealer have a natural BlackJack? " + naturalsCheck(houseHand, true));
         System.out.println("Does Player have a natural BlackJack? " + naturalsCheck(playerHand, false));
+
+        /*
+         * After each hit, calculate the current hand total. If the player busts (> 21), then the game is over
+         * and the house wins. If the player's hand total is < 21, ask if they want to hit or stand
+         */
+        while(playerHand.getHandValue() <= 21 && !playerHand.isStand()){
+            System.out.println("\nHit or stand? Enter 'H' for hit, or 'S' for stand: ");
+            String choice = scanner.nextLine();
+            switch(choice.toUpperCase()){
+                case "S":
+                    playerHand.setStand(true);
+                    break;
+                case "H":
+                    playerHand.hit(deck);
+                    break;
+                default:
+                    System.out.println("Not a valid option");
+                    break;
+            }
+            playerHand.printHand("Player");
+            Collections.shuffle(deck);
+        }
+
+        if(playerHand.getHandValue() > 21){
+            System.out.println("You busted\nCurrent hand value: " + playerHand.getHandValue());
+        }
+
+        /*
+        * Dealer Play
+        * After the Player's turn, the dealer's face-down card is turned up.
+        * If the total is 17 or more, it must stand.
+        * If the total is 16 or under, dealer must take a card and continue until
+        * the hand value is 17 or more, but not over 21.
+        * If the dealer has an ace, and counting it would bring the total to 17 or more
+        * */
+        houseHand.setDealer(false);
+        houseHand.printHand("Dealer");
+
+        while(houseHand.getHandValue() < 17 && !houseHand.isStand()){
+            houseHand.hit(deck);
+            houseHand.printHand("Dealer");
+            Collections.shuffle(deck);
+
+            if(houseHand.getHandValue() >= 17 && houseHand.getHandValue() <= 21){
+                houseHand.setStand(true);
+            }
+        }
+
+
+
+        scanner.close();
 
     }
 
@@ -47,4 +99,6 @@ public class Main {
         }
         return currentHand.getHandValue() == 21;
     }
+
+
 }
